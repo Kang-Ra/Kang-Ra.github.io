5b9e83418edf376454f4ccbf0a3f3c9d1e4c6112
Kang-Ra <belitungraka12@gmail.com>
2026-03-17 11:58:56 +0700

    Add files via upload

---
+++ weblist.js
@@ -0,0 +1,33 @@
+function dropdownmenu(){
+    const ul = document.getElementById("navbar-menu");
+    const close = document.getElementById("logox")
+    const menu = document.getElementById("logomenu")
+    
+    ul.classList.toggle("active");
+    close.classList.toggle("active");
+    menu.classList.toggle("active");
+};
+
+window.addEventListener("scroll", function(){
+const nav = document.querySelector(".navbar")
+
+if(window.scrollY > 90){
+    nav.classList.add("scrolled");
+}
+
+else{
+    nav.classList.remove("scrolled")
+}
+});
+
+function tampilalert(e){
+     e.preventDefault();
+
+    document.getElementById("alert-container").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("alertbox").style.display = "flex";
+
+     setTimeout(function(){
+        document.getElementById("formkontak").submit();
+    }, 2000);
+};
